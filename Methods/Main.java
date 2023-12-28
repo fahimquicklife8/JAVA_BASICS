246fafa6fb09272457e57725e8be2ac97146b6a7
Fahim Imtiaz <90858661+fahimquicklife8@users.noreply.github.com>
2023-12-27 23:41:03 -0600

    Update Main.java

--- Methods/Main.java
+++ Methods/Main.java
@@ -1,1 +1,31 @@
+import java.util.*;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        TreeSet<Integer> tS = new TreeSet<>();
+
+
+        tS.add(50);
+
+        tS.add(5);
+        tS.add(10);
+
+        tS.add(10);
+
+        tS.add(40);
+        tS.add(30);
+        tS.add(90);
+        System.out.println(tS);
+
+
+
+
+
+    }
+    }
