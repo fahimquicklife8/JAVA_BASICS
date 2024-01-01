5d924a0f8b977dcf7f3897f280f85c242d1e92f3
Fahim Imtiaz <90858661+fahimquicklife8@users.noreply.github.com>
2023-12-31 22:37:06 -0600

    Add files via upload

---
+++ Methods/HappyNewyear.java
@@ -0,0 +1,13 @@
+public class HappyNewyear {
+    static void  NewYear(int date, int year){
+
+        System.out.println("Happy New Year");
+    }
+
+    public static void main(String[] args) {
+
+        int date = 31, year = 2023;
+        NewYear(date,year);
+
+    }
+}
