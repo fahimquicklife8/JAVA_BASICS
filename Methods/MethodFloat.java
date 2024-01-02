a3d7977d803535a77170d402debcc0f39125f805
Fahim Imtiaz <90858661+fahimquicklife8@users.noreply.github.com>
2024-01-01 23:37:58 -0600

    Add files via upload

---
+++ Methods/MethodFloat.java
@@ -0,0 +1,18 @@
+public class MethodFloat {
+    static byte max(byte x, byte y){
+        return x>y?x:y;
+    }
+    static int max(int x, int y){
+        return x>y?x:y;
+    }
+    static float max(float x, float y){
+        if(x>y)
+            return x;
+        else
+            return y;
+    }
+    public static void main(String[] args) {
+    byte a = 10, b = 5;
+    System.out.println(max(a,b));
+    }
+}
